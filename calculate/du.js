bb0c3eabe394052c74c45a1df0b319de6dcc7bdc
Deputatov Nikita <deputatovn@yandex.ru>
2019-07-20 19:08:58 +0300

    add du.js

--- calculate/du.js
+++ calculate/du.js
@@ -7,3 +7,19 @@ export default tree =>
     .sort(([, size1], [, size2]) => size2 - size1);
 
 // Teacher's solution
+// const calculatefilesSize = node => reduce((acc, n) => {
+//     if (n.type === 'directory') {
+//       return acc;
+//     }
+
+//     return acc + n.meta.size;
+//   }, node, 0);
+
+//   const du = (node) => {
+//     const result = node.children.map(n => [n.name, calculatefilesSize(n)]);
+//     // Обычный дестракчеринг. JS позволяет пропускать имена если они не используются
+//     result.sort(([, size1], [, size2]) => size2 - size1);
+//     return result;
+//   };
+
+//   export default du;
