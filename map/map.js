bb682e1aea195ceb65fa810602afabf4d9380fdf
Deputatov Nikita <deputatovn@yandex.ru>
2019-07-20 15:42:45 +0300

    add map.js

--- map/map.js
+++ map/map.js
@@ -1,10 +1,10 @@
 const map = (f, node) => {
-  if (node.type === "directory") {
+  if (node.type === 'directory') {
     return { ...f(node), children: node.children.map(c => map(f, c)) };
   }
   return f(node);
 };
-// export default map;
+export default map;
 
 // Teacher's solution
 // const map = (f, node) => {
