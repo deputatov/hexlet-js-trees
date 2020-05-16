ecc23b54fa03327305846354dcb8e98dd64da400
Deputatov Nikita <deputatovn@yandex.ru>
2020-05-16 16:18:47 +0300

    add new solution

--- search/findFilesByName.js
+++ search/findFilesByName.js
@@ -10,3 +10,18 @@ const findFilesByName = (root, substr) => {
 };
 
 export default findFilesByName;
+
+const findFilesByName = (tree, substr) => {
+  const iter = (node, ancestry) => {
+    const name = getName(node);
+    const newAncestry = path.join(ancestry, name);
+    if (isFile(node)) {
+      return name.includes(substr) ? newAncestry : [];
+    }
+    const children = getChildren(node);
+    return children.flatMap((child) => iter(child, newAncestry));
+  };
+  return iter(tree, '');
+};
+
+export default findFilesByName;
